7dd565df47999471c610b09ebf4450bda3a4434b
Jaskaran0702 <21ucs097@lnmiit.ac.in>
2023-12-25 16:47:08 +0530

    latest commit

--- src/background.js
+++ src/background.js
@@ -1,100 +1,44 @@
-// // Define the urlFilter that we want our webNavigation listener to catch
-// // Gets recreated every time the service worker is restarted.
-// // filter = [
-// //     // If the host portion of the user's URL CONTAINS (not matches exactly)
-// //     // any of the following, it will trigger the listener
-// //     { hostContains: "youtube.com" },
-// //     { hostContains: "instagram.com" },
-// //     { hostContains: "twitter.com" },
-// //     { hostContains: "reddit.com" },
-// // ];
-// let filter = [];
-
-// function updateFilter() {
-//     // Retrieve the list of blocked URLs from storage
-//     chrome.storage.sync.get({ allUrls: [] }, function (result) {
-//         filter = result.allUrls.map(url => ({ hostContains: url }));
-//     });
-// }
-
-// // Call updateFilter initially to load the URLs on extension startup
-// updateFilter();
-
-// // When chrome navigates to one of the urls matching our urlFilter...
-// chrome.webNavigation.onBeforeNavigate.addListener(
-//     (details) => {
-//         // Get the current system mode and when the most recent snooze expires
-//         chrome.storage.sync.get(
-//             { mode: false, endSnooze: null },
-//             function (result) {
-//                 // If study mode is ON and our most recent snooze either
-//                 // doesn't exist or the expiration date has already passed...
-//                 if (
-//                     result.mode &&
-//                     (!result.endSnooze ||
-//                         new Date(result.endSnooze) < new Date())
-//                 ) {
-//                     // Update the url of the user's current tab to point to our alternatively defined HTML page
-//                     // Because this depends on the user's browser, we let chrome figure out the exact URL
-//                     chrome.tabs.update(details.tabId, {
-//                         url: chrome.runtime.getURL("/html/altPage.html"),
-//                     });
-//                 }
-//             }
-//         );
-//     },
-//     { url: filter }
-// );
-// Define the urlFilter that we want our webNavigation listener to catch
 let filter = [];
 
 function updateFilter() {
-    // Retrieve the list of blocked URLs from storage
-    chrome.storage.sync.get({ allUrls: [] }, function (result) {
-        filter = result.allUrls.map(url => ({ hostContains: url }));
-    });
+  // Retrieve the list of blocked URLs from storage
+  chrome.storage.sync.get({ allUrls: [] }, function (result) {
+    filter = result.allUrls.map((url) => ({ hostContains: url }));
+  });
 }
 
 // Call updateFilter initially to load the URLs on extension startup
 updateFilter();
 
 // When chrome navigates, check if the URL matches the filter
-chrome.webNavigation.onBeforeNavigate.addListener(
-    (details) => {
-        // Get the current system mode and when the most recent snooze expires
-        chrome.storage.sync.get(
-            { mode: false, endSnooze: null },
-            function (result) {
-                // If study mode is ON and our most recent snooze either
-                // doesn't exist or the expiration date has already passed...
-                if (
-                    result.mode &&
-                    (!result.endSnooze ||
-                        new Date(result.endSnooze) < new Date())
-                ) {
-                    // Check if the URL matches any of the blocked URLs
-                    const isBlocked = filter.some(rule => {
-                        const url = new URL(details.url);
-                        return url.hostname.includes(rule.hostContains);
-                    });
+chrome.webNavigation.onBeforeNavigate.addListener((details) => {
+  // Get the current system mode and when the most recent snooze expires
+  chrome.storage.sync.get({ mode: false, endSnooze: null }, function (result) {
+    // If study mode is ON and our most recent snooze either
+    // doesn't exist or the expiration date has already passed...
+    if (
+      result.mode &&
+      (!result.endSnooze || new Date(result.endSnooze) < new Date())
+    ) {
+      // Check if the URL matches any of the blocked URLs
+      const isBlocked = filter.some((rule) => {
+        const url = new URL(details.url);
+        return url.hostname.includes(rule.hostContains);
+      });
 
-                    // If the URL is blocked, update the tab URL
-                    if (isBlocked) {
-                        chrome.tabs.update(details.tabId, {
-                            url: chrome.runtime.getURL("/html/altPage.html"),
-                        });
-                    }
-                }
-            }
-        );
+      // If the URL is blocked, update the tab URL
+      if (isBlocked) {
+        chrome.tabs.update(details.tabId, {
+          url: chrome.runtime.getURL("/html/altPage.html"),
+        });
+      }
     }
-);
+  });
+});
 
 // Listen for changes to the list of blocked URLs
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        if (request === "updateUrls") {
-            updateFilter();
-        }
-    }
-);
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request === "updateUrls") {
+    updateFilter();
+  }
+});
--- src/options.js
+++ src/options.js
@@ -1,108 +1,3 @@
-// const urlInput = document.getElementById("urlInput");
-
-// const addUrlButton = document.getElementById("addUrlButton");
-
-// const deleteUrlButton = document.getElementById("deleteUrlButton");
-
-// const urlsDiv = document.getElementById("enabledUrls");
-// const clearButton = document.getElementById("clearButton");
-
-// // ---------------------------------------------------------
-
-// let urlsList = [];
-
-// function createUrlItem(urlText) {
-//     const urlItem = document.createElement("p");
-//     urlItem.textContent = urlText;
-//     urlItem.classList.add("urlItem");
-//     document.body.appendChild(urlItem);
-// }
-
-// function printFromStorage() {
-//     chrome.storage.sync.get({ allUrls: [] }, function (result) {
-//         const urlsArray = result.allUrls;
-
-//         if (!urlsArray) return;
-
-//         for (let i = 0; i < urlsArray.length; i++)
-//             console.log("urlsArray[" + i + "] = " + urlsArray[i]);
-//     });
-// }
-
-// function alreadyHaveUrl(url) {
-//     if (urlsList.indexOf(url) !== -1) return true;
-//     return false;
-// }
-
-// // ---------------------------------------------------------
-
-// window.onload = function () {
-//     urlsList = [];
-
-//     chrome.storage.sync.get({ allUrls: [] }, function (result) {
-//         const urlsArray = result.allUrls;
-//         for (let i = 0; i < urlsArray.length; i++) {
-//             createUrlItem(urlsArray[i]);
-//             urlsList.push(urlsArray[i]);
-//         }
-//     });
-// };
-
-// addUrlButton.addEventListener("click", function () {
-//     const newUrlText = urlInput.value.toLowerCase();
-
-//     if (alreadyHaveUrl(newUrlText)) {
-//         alert("That URL is already in the list!");
-//         return;
-//     }
-
-//     createUrlItem(newUrlText);
-//     urlsList.push(newUrlText);
-
-//     chrome.storage.sync.set({ allUrls: urlsList });
-
-//     chrome.runtime.sendMessage("updateUrls", function (response) {
-//         console.log(response ?? "Couldn't update service worker");
-//     });
-
-//     urlInput.value = "";
-// });
-
-// deleteUrlButton.addEventListener("click", function () {
-//     const newUrlText = urlInput.value.toLowerCase();
-
-//     if (!alreadyHaveUrl(newUrlText)) {
-//         alert("That URL is not in the list!");
-//         return;
-//     }
-
-//     urlsList.splice(urlsList.indexOf(newUrlText), 1);
-
-//     const urlItems = document.getElementsByClassName("urlItem");
-//     urlItems[index].remove();
-
-//     chrome.storage.sync.set({ allUrls: urlsList });
-
-//     chrome.runtime.sendMessage("updateUrls", function (response) {
-//         console.log(response ?? "Couldn't update service worker");
-//     });
-
-//     urlInput.value = "";
-// });
-
-// clearButton.addEventListener("click", function () {
-//     chrome.storage.sync.set({ allUrls: [] }, function () {
-//         const urlItems = document.getElementsByClassName("urlItem");
-
-//         for (let i = urlItems.length - 1; i >= 0; i--) urlItems[i].remove();
-
-//         urlsList = [];
-//     });
-
-//     chrome.runtime.sendMessage("updateUrls", function (response) {
-//         console.log(response ?? "Couldn't update service worker");
-//     });
-// });
 const urlInput = document.getElementById("urlInput");
 const addUrlButton = document.getElementById("addUrlButton");
 const deleteUrlButton = document.getElementById("deleteUrlButton");
@@ -112,127 +7,100 @@ const clearButton = document.getElementById("clearButton");
 let urlsList = [];
 
 function createUrlItem(urlText) {
-    const urlItem = document.createElement("p");
-    urlItem.textContent = urlText;
-    urlItem.classList.add("urlItem");
-    urlsDiv.appendChild(urlItem); // Append to the urlsDiv instead of document.body
+  const urlItem = document.createElement("p");
+  urlItem.textContent = urlText;
+  urlItem.classList.add("urlItem");
+  urlsDiv.appendChild(urlItem); // Append to the urlsDiv instead of document.body
 }
 
 function printFromStorage() {
-    chrome.storage.sync.get({ allUrls: [] }, function (result) {
-        const urlsArray = result.allUrls;
+  chrome.storage.sync.get({ allUrls: [] }, function (result) {
+    const urlsArray = result.allUrls;
 
-        if (!urlsArray) return;
+    if (!urlsArray) return;
 
-        for (let i = 0; i < urlsArray.length; i++)
-            console.log("urlsArray[" + i + "] = " + urlsArray[i]);
-    });
+    for (let i = 0; i < urlsArray.length; i++)
+      console.log("urlsArray[" + i + "] = " + urlsArray[i]);
+  });
 }
 
 function alreadyHaveUrl(url) {
-    if (urlsList.indexOf(url) !== -1) return true;
-    return false;
+  if (urlsList.indexOf(url) !== -1) return true;
+  return false;
 }
 
 window.onload = function () {
-    urlsList = [];
+  urlsList = [];
 
-    chrome.storage.sync.get({ allUrls: [] }, function (result) {
-        const urlsArray = result.allUrls;
-        for (let i = 0; i < urlsArray.length; i++) {
-            createUrlItem(urlsArray[i]);
-            urlsList.push(urlsArray[i]);
-        }
-    });
+  chrome.storage.sync.get({ allUrls: [] }, function (result) {
+    const urlsArray = result.allUrls;
+    for (let i = 0; i < urlsArray.length; i++) {
+      createUrlItem(urlsArray[i]);
+      urlsList.push(urlsArray[i]);
+    }
+  });
 };
 
 addUrlButton.addEventListener("click", function () {
-    const newUrlText = urlInput.value.toLowerCase();
+  const newUrlText = urlInput.value.toLowerCase();
 
-    if (alreadyHaveUrl(newUrlText)) {
-        alert("That URL is already in the list!");
-        return;
-    }
+  if (alreadyHaveUrl(newUrlText)) {
+    alert("That URL is already in the list!");
+    return;
+  }
 
-    createUrlItem(newUrlText);
-    urlsList.push(newUrlText);
+  createUrlItem(newUrlText);
+  urlsList.push(newUrlText);
 
-    chrome.storage.sync.set({ allUrls: urlsList });
+  chrome.storage.sync.set({ allUrls: urlsList });
 
-    chrome.runtime.sendMessage("updateUrls", function (response) {
-        console.log(response ?? "Couldn't update service worker");
-    });
+  chrome.runtime.sendMessage("updateUrls", function (response) {
+    console.log(response ?? "Couldn't update service worker");
+  });
 
-    urlInput.value = "";
+  urlInput.value = "";
 });
 
-// deleteUrlButton.addEventListener("click", function () {
-//     const newUrlText = urlInput.value.toLowerCase();
 
-//     if (!alreadyHaveUrl(newUrlText)) {
-//         alert("That URL is not in the list!");
-//         return;
-//     }
-
-//     urlsList.splice(urlsList.indexOf(newUrlText), 1);
-
-//     const urlItems = document.getElementsByClassName("urlItem");
-
-//     for (let i = 0; i < urlItems.length; i++) {
-//         if (urlItems[i].textContent === newUrlText) {
-//             urlItems[i].remove();
-//             break;
-//         }
-//     }
-
-//     chrome.storage.sync.set({ allUrls: urlsList });
-
-//     chrome.runtime.sendMessage("updateUrls", function (response) {
-//         console.log(response ?? "Couldn't update service worker");
-//     });
-
-//     urlInput.value = "";
-// });
 
 deleteUrlButton.addEventListener("click", function () {
-    const newUrlText = urlInput.value.toLowerCase();
+  const newUrlText = urlInput.value.toLowerCase();
 
-    if (!alreadyHaveUrl(newUrlText)) {
-        alert("That URL is not in the list!");
-        return;
+  if (!alreadyHaveUrl(newUrlText)) {
+    alert("That URL is not in the list!");
+    return;
+  }
+
+  urlsList = urlsList.filter((url) => url !== newUrlText);
+
+  const urlItems = document.getElementsByClassName("urlItem");
+
+  for (let i = 0; i < urlItems.length; i++) {
+    if (urlItems[i].textContent === newUrlText) {
+      urlItems[i].remove();
+      break;
     }
+  }
 
-    urlsList = urlsList.filter(url => url !== newUrlText);
+  chrome.storage.sync.set({ allUrls: urlsList });
 
-    const urlItems = document.getElementsByClassName("urlItem");
+  chrome.runtime.sendMessage("updateUrls", function (response) {
+    console.log(response ?? "Couldn't update service worker");
+  });
 
-    for (let i = 0; i < urlItems.length; i++) {
-        if (urlItems[i].textContent === newUrlText) {
-            urlItems[i].remove();
-            break;
-        }
-    }
-
-    chrome.storage.sync.set({ allUrls: urlsList });
-
-    chrome.runtime.sendMessage("updateUrls", function (response) {
-        console.log(response ?? "Couldn't update service worker");
-    });
-
-    urlInput.value = "";
+  urlInput.value = "";
 });
 
-
 clearButton.addEventListener("click", function () {
-    chrome.storage.sync.set({ allUrls: [] }, function () {
-        const urlItems = document.getElementsByClassName("urlItem");
+  chrome.storage.sync.set({ allUrls: [] }, function () {
+    const urlItems = document.getElementsByClassName("urlItem");
 
-        for (let i = urlItems.length - 1; i >= 0; i--) urlItems[i].remove();
+    for (let i = urlItems.length - 1; i >= 0; i--) urlItems[i].remove();
 
-        urlsList = [];
-    });
+    urlsList = [];
+  });
 
-    chrome.runtime.sendMessage("updateUrls", function (response) {
-        console.log(response ?? "Couldn't update service worker");
-    });
+  chrome.runtime.sendMessage("updateUrls", function (response) {
+    console.log(response ?? "Couldn't update service worker");
+  });
 });
